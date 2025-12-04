d290a03563a982e0b69eec70ff92dea33807ee2a
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-12-04 10:39:20 +0530

    Refactor to implement right rotation of array with multiple iteration

--- right rotation single.c
+++ right rotation single.c
@@ -1,14 +1,24 @@
+//c program to perform left rotation of array elements//
 #include<stdio.h>
 #define MAX 100
 int main(){
-    int n,a[MAX],ec=0,odc=0,,i;
+    int n,a[MAX],i,r;
     printf("Enter the size of array: ");
     scanf("%d",&n);
     printf("Enter the elements of array: ");
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    int t=a[i];
+    printf("How many rotation");
+    scanf("%d",&r);
+    for(i=0;i<r;i++){
+        int t=a[0];
+        for(int j=0;j<n;j++){
+           a[j]=a[j+1];
+        }
+        a[n-1]=t;
+    }
+    int t=a[0];
     for(i=0;i<n;i++){
        a[i]=a[i+1];
     }
